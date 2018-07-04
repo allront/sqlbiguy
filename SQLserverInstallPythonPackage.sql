3d3a2c04c38030383bf0f989e74000e78e386beb
Artem Glazkov <allront@mail.ru>
2018-07-04 09:31:53 +0300

    Update SQLserverInstallPythonPackage.sql

--- SQLserverInstallPythonPackage.sql
+++ SQLserverInstallPythonPackage.sql
@@ -1,18 +1,27 @@
--- in this example we install simple_salesforce package
+CREATE PROCEDURE [dbo].[USP_InstallPythonPackage]   (@packageName nvarchar(255)) as
+
+BEGIN
+
 EXECUTE sp_execute_external_script    
-@language = N'Python'    
+@language = N'Python'
 ,@script=
 N'
 def install_and_import(package):
-    import importlib
-    try:
-        importlib.import_module(package)
-    except ImportError:
-        import pip
-        pip.main([''install'', package])
-    finally:
-        globals()[package] = importlib.import_module(package)
+	import importlib
+	try:
+		importlib.import_module(package)
+	except ImportError:
+		import pip
+		pip.main([''install'', package])
+	finally:
+		globals()[package] = importlib.import_module(package)
 
-install_and_import(''simple_salesforce'')
+install_and_import(packageName)
 '
--- to be done in future - create tsql proc, and tranfert package name into sp_xecute_external_script proc
+, @params = N'@packageName nvarchar(255)'  
+, @packageName = @packageName
+
+END
+
+-- to call USP for e.g. install simple_salesforce package type [tech].[USP_InstallPythonPackage] 'simple_salesforce'
+
